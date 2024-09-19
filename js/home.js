57fe01bd015f7e45372a58a1d447ca2168d8303e
rimiruma <rimiruma12@gmail.com>
2024-09-19 22:26:24 +0600

    added money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home added');
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    const addPinInput = document.getElementById('input-pin-number').value;
+    console.log(addPinInput);
+    
+    
+    
+})
