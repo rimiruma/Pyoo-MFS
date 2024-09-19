7df7705b48c9c134f6900485b13d54a7a900bf5e
rimiruma <rimiruma12@gmail.com>
2024-09-19 14:31:03 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -3,9 +3,19 @@
 // form submit reloading the page google search
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('login button click');
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    if(phoneNumber==='5' && pinNumber ==='1234'){
+        console.log('You are logged in');
+        window.location.href = '/home.html'
+        
+    }
+    else{
+        alert('Wrong phone or pin');
+        
+    }
     
 })
 
